cb35148d029218260f8e681c4606639a72fb2987
abarrachina <abarrachina@ximdex.com>
2014-04-10 13:10:58 +0200

    Adding class to change modules installation state

--- inc/install/steps/ximdexmodules/js/InstallModulesController.js
+++ inc/install/steps/ximdexmodules/js/InstallModulesController.js
@@ -66,7 +66,6 @@ ximdexInstallerApp.controller('InstallModulesController', ['$scope', 'installerS
 			installerService.sendAction("installModule","module="+module.name).then(function(response) {
 		        $scope.modules[index]["processed"]=true;
 		        $scope.modules[index]["state"]=response.data.result;
-                $scope.modules[index]["classState"]="module-"+response.data.result;
 		        index++;
 		        $scope.installModule(index);
 		    });
