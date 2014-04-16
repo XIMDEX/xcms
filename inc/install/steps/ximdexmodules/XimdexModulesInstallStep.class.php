db3874ef1c2ef4532a064adba1be143ea6a5a25f
abarrachina <abarrachina@ximdex.com>
2014-04-16 09:36:01 +0200

    Solving include behaviour

--- inc/install/steps/ximdexmodules/XimdexModulesInstallStep.class.php
+++ inc/install/steps/ximdexmodules/XimdexModulesInstallStep.class.php
@@ -40,7 +40,7 @@ class XimdexModulesInstallStep extends GenericInstallStep {
 		
 		$this->addJs("InstallModulesController.js");
 		$imManager = new InstallModulesManager(InstallModulesManager::WEB_MODE);
-		$result = $imManager->buildModulesFile();
+		$result = $imManager->buildModulesFile();		
 		if (!$result)
 			$this->exception[]="Unavailable install modules. Do you have right permission on install/install-modules.conf?";
 		$this->render();
@@ -50,18 +50,10 @@ class XimdexModulesInstallStep extends GenericInstallStep {
 	 * List all none default modules and send a json object
 	 */
 	public function getModulesLikeJson(){
-
+		
 		$imManager = new InstallModulesManager(InstallModulesManager::WEB_MODE);
 		$ftManager = new FastTraverseManager (FastTraverseManager::WEB_MODE);		
-		$ftManager->buildFastTraverse();
-		$modules = $this->installManager->getModulesByDefault();
-		include(XIMDEX_ROOT_PATH."/conf/install-modules.conf");
-		foreach ($modules as $module) {
-			$imManager->installModule($module["name"]);
-		}
-		foreach ($modules as $module) {
-			$imManager->enableModule($module["name"]);
-		}
+		$ftManager->buildFastTraverse();		
 		$modules = $this->installManager->getModulesByDefault(false);		
 		$this->sendJSON($modules);
 	}
@@ -74,6 +66,7 @@ class XimdexModulesInstallStep extends GenericInstallStep {
 		$moduleName = $this->request->getParam("module");
 		$imManager = new InstallModulesManager(InstallModulesManager::WEB_MODE);
 		$installState = $imManager->installModule($moduleName);
+		$imManager->enableModule($moduleName);
 		$values=array("result"=>strtolower($installState));
 		$this->sendJSON($values);
 				
