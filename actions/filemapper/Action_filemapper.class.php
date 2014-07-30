22aa9b5e7d9ef11be6439c480ed4d8abbc185f09
Jesús Vargas <estudiantejm@gmail.com>
2014-07-30 09:09:15 +0200

    Fixed problem downloading opendocument files and fixed the view of managefolders

--- actions/filemapper/Action_filemapper.class.php
+++ actions/filemapper/Action_filemapper.class.php
@@ -26,6 +26,7 @@
 
 
 ModulesManager::file('/inc/repository/nodeviews/View_FilterMacros.class.php');
+ModulesManager::file('/actions/filedownload_multiple/Action_filedownload_multiple.class.php');
 
 class Action_filemapper extends ActionAbstract {
    // Main method: shows initial form
@@ -52,23 +53,69 @@ class Action_filemapper extends ActionAbstract {
     private function echoNode($idNode){
     	$fileNode = new Node($idNode);
 		$fileName = $fileNode->get('Name');
-		$fileContent = $fileNode->GetContent();
-		$gmDate =  gmdate("D, d M Y H:i:s");
+        $gmDate =  gmdate("D, d M Y H:i:s");
 
-		/// Expiration headers
-		$this->response->set('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
-		$this->response->set('Last-Modified', $gmDate . " GMT");
-		$this->response->set('Cache-Control', 
-			array('no-store, no-cache, must-revalidate', 'post-check=0, pre-check=0'));
-		$this->response->set('Pragma', 'no-cache');
-		$this->response->set('ETag', md5($idNode.$gmDate));
-		$this->response->set('Content-Length', strlen(strval($fileContent)));
-		$this->response->set('Content-transfer-encoding', 'binary');
-		$this->response->set('Content-type', 'octet/stream');
-		$this->response->set('Content-Disposition', "attachment; filename=".$fileName);
-		$this->response->sendHeaders();
-		/// Content headers
-		echo $fileContent;
+        /// Expiration headers
+        $this->response->set('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
+        $this->response->set('Last-Modified', $gmDate . " GMT");
+        $this->response->set('Cache-Control',
+            array('no-store, no-cache, must-revalidate', 'post-check=0, pre-check=0'));
+        $this->response->set('Pragma', 'no-cache');
+        $this->response->set('ETag', md5($idNode.$gmDate));
+        $this->response->set('Content-transfer-encoding', 'binary');
+        $this->response->set('Content-type', 'octet/stream');
+        $this->response->set('Content-Disposition', "attachment; filename=".$fileName);
+
+        $df=new DataFactory($idNode);
+        $versionID = $df->GetLastVersion();
+        if (!is_null($versionID)) {
+            $subVersion = $df->GetLastSubVersion($versionID);
+        }
+
+        if (!(!(is_null($versionID)) && !(is_null($subVersion)))) {
+            XMD_Log::warning('No se ha podido estimar la versi�n o la subversion');
+            return false;
+        }
+
+        $uniqueName = $df->GetTmpFile($versionID, $subVersion);
+
+        if(!$uniqueName) {
+            XMD_Log::warning('No se ha podido obtener el file');
+            $this->SetError(3);
+            return false;
+        }
+
+        $targetPath = Config::getValue("AppRoot") . Config::getValue("FileRoot"). "/". $uniqueName;
+        $path_parts = pathinfo($targetPath);
+        $fileNameWithoutExtension=$path_parts['filename'];
+        if(strpos(mime_content_type($targetPath),"application/vnd.oasis.opendocument.") !== false){
+            $tmpFolder = FsUtils::getUniqueFolder(
+                Config::getValue('AppRoot') . Config::getValue('TempRoot'), '', 'export_'
+            );
+            if (!FsUtils::mkdir($tmpFolder)) {
+                XMD_Log::error(_('A temporal directory to export could not be created'));
+                return;
+            }
+            if (!FsUtils::file_put_contents($tmpFolder.'/'.$fileName, $fileNode->getContent())) {
+                $fileTemp = $fileName;
+            }
+            $tarName = sprintf('%s.zip', $fileNameWithoutExtension);
+            exec(sprintf('zip -jr %s %s', dirname($tmpFolder)."/".$tarName, $tmpFolder.'/'.$fileName));
+            $ret = FsUtils::deltree($tmpFolder);
+            if (!$ret) {
+                XMD_Log::info(_("Directory could not be deleted ").$tmpFolder);
+            }
+            $this->response->set('Content-Disposition', "attachment; filename=".preg_replace('/\\.[^.\\s]{3,4}$/', '', $fileName).".zip");
+            $this->response->sendHeaders();
+            readfile(dirname($tmpFolder).'/'.$tarName);
+            exit();
+        }else{
+            $fileContent = $fileNode->GetContent();
+            $this->response->set('Content-Disposition', "attachment; filename=".$fileName);
+            $this->response->set('Content-Length', strlen(strval($fileContent)));
+            $this->response->sendHeaders();
+            echo $fileContent;
+        }
     }
 }
 ?>
