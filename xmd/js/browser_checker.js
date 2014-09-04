91f26ab8c8c43d02fefdaf20937db3c0100d7805
eramirezr <eramirez@lab08.ximdex.net>
2014-09-04 11:19:24 +0200

    Fixing height for iframe at preview action.

--- xmd/js/browser_checker.js
+++ xmd/js/browser_checker.js
@@ -65,8 +65,8 @@ function capLock(e){
     kc = e.keyCode?e.keyCode:e.which;
     sk = e.shiftKey?e.shiftKey:((kc == 16)?true:false);
     if(((kc >= 65 && kc <= 90) && !sk)||((kc >= 97 && kc <= 122) && sk))
-        document.getElementById('capsLockAdvice').style.visibility = 'visible';
+        $('#capsLockAdvice').addClass('show-msg');
     else
-        document.getElementById('capsLockAdvice').style.visibility = 'hidden';
+        $('#capsLockAdvice').removeClass('show-msg');
 }
 
