d9679940a9de1a9939bb433bf66d089c5981be24
jvargas91 <jvargas@ximdex.com>
2015-01-26 14:20:14 +0100

    Fixed splash screen

--- actions/browser3/resources/js/XMainCtrl.js
+++ actions/browser3/resources/js/XMainCtrl.js
@@ -1,4 +1,10 @@
 angular.module('ximdex.main.controller')
-	.controller('XMainCtrl', ['$scope', '$attrs', 'xEventRelay', function($scope, $attrs, xEventRelay){
-
+	.controller('XMainCtrl', ['$scope', '$attrs', 'xEventRelay', '$timeout', function($scope, $attrs, xEventRelay, $timeout){
+        $timeout(
+                function() {
+                    angular.element("#ximdex-splash").remove();
+                }
+            ,
+                3200
+        );
 	}]);
