45144e70eb8bcc2efe2b1d44feafb40909621f2d
alsanchez86 <sanchezpizarro86@gmail.com>
2016-01-28 15:02:31 +0100

    use getters and setters Node class

--- lib/Ximdex/Models/Node.php
+++ lib/Ximdex/Models/Node.php
@@ -793,8 +793,8 @@ class Node extends NodesOrm
     {
         $this->ClearError();
         if ($this->get('IdNode') > 0) {
-            if ($this->nodeType->get('IsRenderizable')) {
-                if ($this->nodeType->get('HasFSEntity')) {
+            if ($this->nodeType->GetIsRenderizable()) {
+                if ($this->nodeType->GetHasFSEntity()) {
                     $this->class->RenderizeNode();
                 }
                 if ($recursive) {
