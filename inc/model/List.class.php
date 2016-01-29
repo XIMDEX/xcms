05cbf23e35bb46413dc0a284d347808a4f6b9d4a
DrZippie <antonio@antoniocortes.com>
2016-01-29 21:25:19 +0100

    Moved multiple files to new Namespace

--- inc/model/List.class.php
+++ inc/model/List.class.php
@@ -25,13 +25,9 @@
  */
 
 
+use Ximdex\Models\ORM\ListOrm;
 
-if (!defined('XIMDEX_ROOT_PATH')) {
-	define ('XIMDEX_ROOT_PATH', realpath(dirname(__FILE__)) . '/../..');
-}
 
-require_once XIMDEX_ROOT_PATH . '/inc/model/orm/List_ORM.class.php';
-
-class List_ extends List_ORM {
+class List_ extends ListOrm {
 	
 }
