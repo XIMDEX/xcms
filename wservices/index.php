7a910d2057e2e437648acde300f9da4e0df78be3
DrZippie <antonio@antoniocortes.com>
2016-02-25 10:15:38 +0100

    Added Login and logout to service

--- wservices/index.php
+++ wservices/index.php
@@ -14,19 +14,19 @@ use Ximdex\Utils\Session;
 
 include_once '../bootstrap/start.php';
 
-$router = new Router( new Request()  ) ;
+$router = new Router(new Request());
 
-$router->addAllowedRequest( "ping") ;
+$router->addAllowedRequest("ping");
 
-$router->addRoute( 'bad', "none" );
+$router->addRoute('bad', "none");
 
-$router->addAllowedRequest( "bad") ;
+$router->addAllowedRequest("bad");
 
 
-$router->addRoute( 'ping', function(Request $r, Response $w ) {
-    $w->setStatus( 0) ;
-    $w->setMessage( "" ) ;
-    $w->setResponse( "PONG!") ;
+$router->addRoute('ping', function (Request $r, Response $w) {
+    $w->setStatus(0);
+    $w->setMessage("");
+    $w->setResponse("PONG!");
     $w->send();
 });
 
@@ -35,7 +35,7 @@ $router->addRoute( 'ping', function(Request $r, Response $w ) {
  * @param offset (optional) The offset
  * @param limit (optional) The max number of results
  */
-$router->addRoute('search', function(Request $r, Response $w){
+$router->addRoute('search', function (Request $r, Response $w) {
     $q = $r->get('q');
     ModulesManager::file('/src/SolrSearchManager.php', 'XSearch');
 
@@ -48,12 +48,12 @@ $router->addRoute('search', function(Request $r, Response $w){
 
     $resultsToSend = [];
 
-    foreach($results['docs'] as $k => $result){
+    foreach ($results['docs'] as $k => $result) {
         $resultToSend = [
             'id' => $result['IdNode'],
             'name' => $result['Name'],
         ];
-        switch($result['IdNodeType']){
+        switch ($result['IdNodeType']) {
             case NodeType::XSIR_IMAGE_FILE:
                 $resultToSend['type'] = 'image';
                 break;
@@ -81,8 +81,8 @@ $router->addRoute('search', function(Request $r, Response $w){
 /**
  * Route to get user info
  */
-$router->addRoute('me', function(Request $r, Response $w){
-    $userID = (int) Session::get('userID');
+$router->addRoute('me', function (Request $r, Response $w) {
+    $userID = (int)Session::get('userID');
     $user = new User($userID);
     $locale = $user->get('Locale');
     $locale = !is_null($locale) ? $locale : 'en_US';
@@ -102,7 +102,7 @@ $router->addRoute('me', function(Request $r, Response $w){
  * @param offset (optional) The offset
  * @param limit (optional) The max number of results
  */
-$router->addRoute('books', function(Request $r, Response $w){
+$router->addRoute('books', function (Request $r, Response $w) {
     ModulesManager::file('/actions/composer/Action_composer.class.php');
 
     $offset = $r->get('offset', true, 0);
@@ -112,10 +112,10 @@ $router->addRoute('books', function(Request $r, Response $w){
     $resp = $ac->quickReadWithNodetype(10000, NodeType::XBUK_PROJECT, $offset, $limit, null, 1);
 
     $booksToSend = [];
-    if(isset($resp["collection"])){
-        foreach($resp["collection"] as $node){
+    if (isset($resp["collection"])) {
+        foreach ($resp["collection"] as $node) {
             $booksToSend[] = [
-                'id' => (int) $node['nodeid'],
+                'id' => (int)$node['nodeid'],
                 'name' => $node['name'],
             ];
         }
@@ -131,11 +131,11 @@ $router->addRoute('books', function(Request $r, Response $w){
  * @param offset (optional) The offset
  * @param limit (optional) The max number of results
  */
-$router->addRoute('book/\d+/sections', function(Request $r, Response $w){
-    $pathElements = explode( "/", $r->getPath() ) ;
+$router->addRoute('book/\d+/sections', function (Request $r, Response $w) {
+    $pathElements = explode("/", $r->getPath());
     $nodeId = $pathElements[1];
     $node = new Node($nodeId);
-    if($node->GetNodeType() != NodeType::XBUK_PROJECT){
+    if ($node->GetNodeType() != NodeType::XBUK_PROJECT) {
         $w->setMessage('Id is not for a valid book project')->setStatus(1);
         return;
     }
@@ -149,10 +149,10 @@ $router->addRoute('book/\d+/sections', function(Request $r, Response $w){
     $resp = $ac->quickReadWithNodetype($nodeId, NodeType::XBUK_SESSION, $offset, $limit, null, 1);
 
     $booksToSend = [];
-    if(isset($resp["collection"])){
-        foreach($resp["collection"] as $node){
+    if (isset($resp["collection"])) {
+        foreach ($resp["collection"] as $node) {
             $booksToSend[] = [
-                'id' => (int) $node['nodeid'],
+                'id' => (int)$node['nodeid'],
                 'name' => $node['name'],
             ];
         }
@@ -168,14 +168,14 @@ $router->addRoute('book/\d+/sections', function(Request $r, Response $w){
  * @param id The DAM node id
  */
 
-$router->addRoute('DAM/\d+/info', function(Request $r, Response $w){
+$router->addRoute('DAM/\d+/info', function (Request $r, Response $w) {
 
-    $pathElements = explode( "/", $r->getPath() ) ;
+    $pathElements = explode("/", $r->getPath());
     $nodeId = $pathElements[1];
     $node = new Node($nodeId);
 
     $allowedNodetypes = [NodeType::XSIR_BINARY_FILE, NodeType::XSIR_IMAGE_FILE, NodeType::XSIR_VIDEO_FILE, NodeType::XSIR_TEXT_FILE, NodeType::XSIR_WIDGET_FILE];
-    if(!in_array($node->GetNodeType(), $allowedNodetypes)){
+    if (!in_array($node->GetNodeType(), $allowedNodetypes)) {
         $w->setMessage('Id is not for a valid DAM file')->setStatus(1);
         return;
     }
@@ -186,7 +186,7 @@ $router->addRoute('DAM/\d+/info', function(Request $r, Response $w){
 
     $resp['url'] = App::get('UrlRoot') . '/data/files/' . $info['File'];
 
-    $resp['id'] = (int) $node->IdNode;
+    $resp['id'] = (int)$node->IdNode;
 
     $resp['name'] = $node->Name;
 
@@ -206,10 +206,10 @@ $router->addRoute('DAM/\d+/info', function(Request $r, Response $w){
             $custom_info = $xpathObj->query("//custom_info/*");
             if ($custom_info->length > 0) {
                 foreach ($custom_info as $value) {
-                    if(!isset($resp['metadata'])){
+                    if (!isset($resp['metadata'])) {
                         $resp['metadata'] = [];
                     }
-                    if(!isset($resp['metadata'][$langIsoName])){
+                    if (!isset($resp['metadata'][$langIsoName])) {
                         $resp['metadata'][$langIsoName] = [];
                     }
                     $resp['metadata'][$langIsoName][$value->nodeName] = $value->nodeValue;
@@ -240,24 +240,24 @@ $router->addRoute('DAM/\d+/info', function(Request $r, Response $w){
  * Get the users related with a book
  * @param id The book id
  */
-$router->addRoute('book/\d+/users', function(Request $r, Response $w){
-    $pathElements = explode( "/", $r->getPath() ) ;
+$router->addRoute('book/\d+/users', function (Request $r, Response $w) {
+    $pathElements = explode("/", $r->getPath());
     $nodeId = $pathElements[1];
     $node = new Node($nodeId);
 
-    if($node->GetNodeType() != NodeType::XBUK_PROJECT){
+    if ($node->GetNodeType() != NodeType::XBUK_PROJECT) {
         throw new APIException('Id is not for a valid book', 1);
     }
 
     $groups = $node->GetGroupList();
 
     // We won't use general group
-    if(($key = array_search('101', $groups)) !== false) {
+    if (($key = array_search('101', $groups)) !== false) {
         array_splice($groups, $key, 1);
     }
 
     $idUsers = [];
-    foreach($groups as $q){
+    foreach ($groups as $q) {
         $group = new Group($q);
         $idUsers = array_merge($idUsers, $group->GetUserList());
     }
@@ -265,17 +265,17 @@ $router->addRoute('book/\d+/users', function(Request $r, Response $w){
 
     //Check if I have permissions
     $myUserID = Session::get('userID');
-    if(!in_array($myUserID, $idUsers)){
+    if (!in_array($myUserID, $idUsers)) {
         throw new APIException('You haven\'t got enough permissions to see this', -2);
     }
 
     $users = [];
-    foreach($idUsers as $idUser){
+    foreach ($idUsers as $idUser) {
         $user = new User($idUser);
         $locale = $user->get('Locale');
         $locale = !is_null($locale) ? $locale : 'en_US';
         $users [] = [
-            'id' => (int) $idUser,
+            'id' => (int)$idUser,
             'username' => $user->get('Login'),
             'name' => $user->get('Name'),
             'email' => $user->get('Email'),
@@ -287,15 +287,47 @@ $router->addRoute('book/\d+/users', function(Request $r, Response $w){
     $w->send();
 });
 
-$router->addRoute( "login" ,function( Request $request, Response $response )  {
 
-    $response->setResponse( "TEST");
+/**
+ *
+ * Login request
+ *
+ */
+$router->addAllowedRequest("login");
+$router->addRoute("login", function (Request $request, Response $response) {
+
+    $auth = new \Ximdex\Authenticator();
+
+
+    $user = $request->get("user");
+    $pwd = $request->get("pwd");
+
+
+    $response->setStatus(-1);
+    $response->setMessage("Bad Credentials");
+    if ($auth->login($user, $pwd)) {
+        $response->setStatus(0);
+        $response->setMessage("Auth:login");
+    }
     $response->send();
-
-
-
 
 });
 
- $router->execute() ;
+/**
+ *
+ * Login request
+ *
+ */
+$router->addAllowedRequest("logout");
+$router->addRoute("logout", function (Request $request, Response $response) {
+
+    $auth = new \Ximdex\Authenticator();
+    $auth->logout();
+    $response->setStatus(-1);
+    $response->setMessage("Logged out");
+    $response->send();
+
+});
+
+$router->execute();
 
