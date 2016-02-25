23c079dc8fb017e4d108e99ca94dd58f5e3f91d4
DrZippie <antonio@antoniocortes.com>
2016-02-25 13:55:23 +0100

    added Access-Control-Allow-Credentials: true

--- lib/Ximdex/API/Response.php
+++ lib/Ximdex/API/Response.php
@@ -66,6 +66,7 @@ class Response
         ];
         // TODO: Check CORS and filters 
         header( "Access-Control-Allow-Origin: *");
+        header( "Access-Control-Allow-Credentials: true");
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($data, JSON_UNESCAPED_UNICODE);
         exit ;
