c6733281fd15940c7e1ff6123bbebb5aa93e7cd3
DrZippie <antonio@antoniocortes.com>
2016-05-28 16:04:56 +0200

    removed unused files

--- install/ximdex_data/db_diffs/2014-10-28.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `Config` VALUES (NULL, 'MaxItemsPerGroup',50);
--- install/ximdex_data/db_diffs/2014-10-31.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `Channels` ADD COLUMN `Default_Channel` boolean NOT NULL default FALSE;
-UPDATE `Channels` SET `Default_Channel`=1 WHERE `IdChannel`=10001;
