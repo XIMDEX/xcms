a548f33cbe8a516e3b76c704c479816f84fd45b1
DrZippie <antonio@antoniocortes.com>
2016-06-10 11:51:17 +0200

    added script to schedule

---
+++ tasks/scheduler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+
+BASEDIR="$( dirname `dirname "$SOURCE"` )"
+SCRIPT="modules/ximSYNC/scripts/scheduler/scheduler.php"
+LOG="logs/scheduler.log"
+
+
+while :
+    do
+        $BASEDIR/$SCRIPT  >> $BASEDIR/$LOG 2>&1
+done
