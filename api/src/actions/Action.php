fa7a68e2477887e7829de352f60f020ee1853f13
David Arroyo <darroyo@ximdex.com>
2018-02-23 08:23:40 +0100

    Added abstract class Action with helpers methods

---
+++ api/src/actions/Action.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jvargas
+ * Date: 19/02/16
+ * Time: 14:39
+ */
+
+namespace XimdexApi\actions;
+
+use XimdexApi\core\Router;
+
+
+abstract class Action
+{
+
+    protected const PREFIX = '';
+    protected const ROUTES = [];
+    protected const PUBLIC = [];
+
+    public static function addMethods(Router $router)
+    {
+        foreach (static::ROUTES as $route => $action) {
+            $router->addRoute(static::getPath($route), [static::class, $action]);
+
+            if (in_array($action, static::PUBLIC)) {
+                $router->addAllowedRequest($action);
+            }
+        }
+    }
+
+    protected static function getPath($action)
+    {
+        return (!empty(static::PREFIX) ? static::PREFIX . '/' : '') . $action;
+    }
+}
