d4d15d8febc3594d729c869fdcf83781513edef6
Antonio Jesús Lucena <ajlucena@ximdex.com>
2018-03-05 13:04:17 +0100

    Load mime type for every node content required to be previewed

--- public_xmd/actions/filemapper/Action_filemapper.class.php
+++ public_xmd/actions/filemapper/Action_filemapper.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *  \details &copy; 2011  Open Ximdex Evolution SL [http://www.ximdex.org]
  *
@@ -28,30 +29,29 @@ use Ximdex\Logger;
 use Ximdex\Models\Node;
 use Ximdex\MVC\ActionAbstract;
 use Ximdex\Parsers\ParsingPathTo;
+use Ximdex\Runtime\App;
+use Ximdex\Models\Version;
 
-class Action_filemapper extends ActionAbstract {
-    
-   // Main method: shows initial form
-    function index() {
-        
-		if ($this->request->getParam('nodeid')) {
-		    
-			$idNode = $this->request->getParam("nodeid");
+class Action_filemapper extends ActionAbstract
+{
+    // Main method: shows initial form
+    function index()
+    {
+        if ($this->request->getParam('nodeid')) {           
+            $idNode = $this->request->getParam("nodeid");
             $this->echoNode($idNode);
-		}
+        }
     }
 
-    public function nodeFromExpresion(){
-        
+    public function nodeFromExpresion()
+    {
         if ($this->request->getParam('expresion')) {
-            
             $expression = $this->request->getParam("expresion");
             Logger::setActiveLog('preview');
             Logger::debug('Call to filemapper->nodeFromExpresion(Expresion: ' . $expression . ')');
             $parserPathTo = new ParsingPathTo();
             $parserPathTo->parsePathTo($expression);
-            if ($parserPathTo->getIdNode()){
-                
+            if ($parserPathTo->getIdNode()) {
                 $idNode = $parserPathTo->getIdNode();
                 Logger::debug('Calling to filemapper->echoNode(' . $idNode . ')');
                 $this->echoNode($idNode);
@@ -60,25 +60,55 @@ class Action_filemapper extends ActionAbstract {
         }
     }
 
-    private function echoNode($idNode){
-        
-    	$fileNode = new Node($idNode);
-		$fileName = $fileNode->get('Name');
-		
-		Logger::debug('Procesing filemapper->echoNode(' . $idNode . '): filename: ' . $fileName);
-		
-        $gmDate =  gmdate("D, d M Y H:i:s");
+    private function echoNode($idNode)
+    {
+        $fileNode = new Node($idNode);
+        $fileName = $fileNode->get('Name');
+        Logger::debug('Procesing filemapper->echoNode(' . $idNode . '): filename: ' . $fileName);
+        $gmDate = gmdate("D, d M Y H:i:s");
         $fileContent = $fileNode->GetContent();
         
-        /// Expiration headers
+        // Expiration headers
         $this->response->set('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
         $this->response->set('Last-Modified', $gmDate . " GMT");
-        $this->response->set('Cache-Control', array('no-store, no-cache, must-revalidate', 'post-check=0, pre-check=0'));
+        $this->response->set('Cache-Control', array(
+            'no-store, no-cache, must-revalidate',
+            'post-check=0, pre-check=0'
+        ));
         $this->response->set('Pragma', 'no-cache');
-        $this->response->set('ETag', md5($idNode.$gmDate));
+        $this->response->set('ETag', md5($idNode . $gmDate));
         $this->response->set('Content-transfer-encoding', 'binary');
-        $this->response->set('Content-type', 'text/css');
-        $this->response->set('Content-Disposition', "attachment; filename=".$fileName);
+        
+        // Get the mime type of the file
+        $info = pathinfo($fileName);
+        if (strtolower($info['extension']) == 'css') {
+            
+            // CSS files return text/plain by default
+            $mimeType = 'text/css';
+        }
+        else {
+            
+            // Obtain the mime type from the last version of the file
+            $version = $fileNode->GetLastVersion();
+            if (!isset($version['IdVersion']) or !$version['IdVersion']) {
+                Logger::error('There is no a version for node: ' . $idNode);
+                return false;
+            }
+            $versionID = $version['IdVersion'];
+            $version = new Version($versionID);
+            $file = XIMDEX_ROOT_PATH . App::getValue('FileRoot') . '/' . $version->get('File');
+            if (!file_exists($file)) {
+                Logger::error('Cannot load the file: ' . $file . ' for version: ' . $versionID);
+                return false;
+            }
+            $mimeType = mime_content_type($file);
+            if (!$mimeType) {
+                Logger::error('Cannot load the mime type for the file: ' . $file);
+                return false;
+            }
+        }
+        $this->response->set('Content-type', $mimeType);
+        $this->response->set('Content-Disposition', "attachment; filename=" . $fileName);
         $this->response->set('Content-Length', strlen(strval($fileContent)));
         $this->response->sendHeaders();
         echo $fileContent;
